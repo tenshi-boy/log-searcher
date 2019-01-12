a05f00ab208ddea681c8e6e7fdbc6ad9ae927236
tenshi-boy <stanislawchics@gmail.com>
2019-01-12 19:52:43 +0300

    Добавил класс NavigationPanel панель с поиском вхождений и кнопками навигации и класс ReplaceWindow - диалоговое окно осуществляющее замену

--- src/my/log/searcher/LogStarter.java
+++ src/my/log/searcher/LogStarter.java
@@ -1,8 +1,11 @@
 package my.log.searcher;
 
+import java.io.File;
+
 public class LogStarter {
     public static void main(String[] args)
     {
         new MainWindow();
+      //  new ReplaceWindow(new TextViewer(new File("asd")));
     }
 }
