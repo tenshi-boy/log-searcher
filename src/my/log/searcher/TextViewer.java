9a300c520769a21cb23ed75ced08d1671339d879
tenshi-boy <stanislawchics@gmail.com>
2019-01-21 02:57:49 +0300

    Теперь кнопка "Назад" класса NavigationPanel становится активной, после того как весь контент загружен в textArea

--- src/my/log/searcher/TextViewer.java
+++ src/my/log/searcher/TextViewer.java
@@ -25,115 +25,120 @@ public class TextViewer extends JPanel {
     NavigationPanel navPanel;
     JLabel infoLabel;
     //количество символов, добавляемых при прокрутке до 95% JScrollPane
-    int step=5000000;
-    int startSymbol=0;
-    int finishSymbol=step;
-    boolean endOfFile=false;
-    boolean contentChanged=false;
+    int step = 5000000;
+    int startSymbol = 0;
+    int finishSymbol = step;
+    boolean endOfFile = false;
+    boolean contentChanged = false;
     //содержимое файла сразу загружается в fileContent
     String fileContent;
 
     //delayIsOver и artificialDelay используются для создания задержки при добавлении контента в JTextArea
-    boolean delayIsOver=true;
-    Runnable artificialDelay=new ArtificialDelay(this);
+    boolean delayIsOver = true;
+    Runnable artificialDelay = new ArtificialDelay(this);
 
     /**
      * Конструктор TextViewer, при открытии файла размещает на себе JLabel, используемый для отображения загрузки
      * и запускает поток readFileThread, который считывает файл в fileContent
+     *
      * @param file открываемый файл
      */
-    public TextViewer(File file){
-        openedFile=file;
+    public TextViewer(File file) {
+        openedFile = file;
         setLayout(new BorderLayout());
         try {
             ImageIcon imageIcon = new ImageIcon(this.getClass().getResource("/img/loading.gif"));
             loading = new JLabel(imageIcon);
         } catch (Exception e) {
             loading = new JLabel("Ожидайте...");
-        } finally{
+        } finally {
             add(BorderLayout.CENTER, loading);
         }
-        Runnable readFile = new ReadingFileThread(file,this);
+        Runnable readFile = new ReadingFileThread(file, this);
         Thread readFileThread = new Thread(readFile);
         readFileThread.start();
     }
 
     /**
      * Метод вставляет очередную порцию контента в JTextArea
-     * @param start - начиная с символа
+     *
+     * @param start  - начиная с символа
      * @param finish - до символа
      */
-    public void insertContentPart(int start,int finish){
-        if(scrollPane==null){
+    public void insertContentPart(int start, int finish) {
+        if (scrollPane == null) {
             remove(loading);
 
-            textArea=new JTextArea();
+            textArea = new JTextArea();
             textArea.getDocument().addDocumentListener(new ContentChangedTrue());
-            scrollPane=new JScrollPane(textArea);
-            add(BorderLayout.CENTER,scrollPane);
+            scrollPane = new JScrollPane(textArea);
+            add(BorderLayout.CENTER, scrollPane);
             scrollPane.getVerticalScrollBar().addAdjustmentListener(new appendNewContentTextArea());
 
-            navPanel= new NavigationPanel(this);
-            add(BorderLayout.NORTH,navPanel);
+            navPanel = new NavigationPanel(this);
+            add(BorderLayout.NORTH, navPanel);
 
-            infoLabel= new JLabel("");
-            add(BorderLayout.SOUTH,infoLabel);
+            infoLabel = new JLabel("");
+            add(BorderLayout.SOUTH, infoLabel);
             revalidate();
         }
-        if(finish>=fileContent.length()) {
+        if (finish >= fileContent.length()) {
             finish = fileContent.length();
-            endOfFile=true;
+            endOfFile = true;
+            navPanel.prevBtn.setEnabled(true);
             infoLabel.setText("");
-        }else{
+        } else {
             infoLabel.setText("Внимание! Файл загружен не полностью, и будет подгружаться по мере просмотра");
         }
-        if(start<0)
-            start=0;
-        boolean localContentChanged=contentChanged;
-        textArea.append(fileContent.substring(start,finish));
-        contentChanged=localContentChanged;
+        if (start < 0)
+            start = 0;
+        boolean localContentChanged = contentChanged;
+        textArea.append(fileContent.substring(start, finish));
+        contentChanged = localContentChanged;
     }
+
     /**
      * Обработчик, вставляющий контент в TextArea
      */
     class appendNewContentTextArea implements AdjustmentListener {
         @Override
         public void adjustmentValueChanged(AdjustmentEvent e) {
-            if(endOfFile){
+            if (endOfFile) {
                 scrollPane.getVerticalScrollBar().removeAdjustmentListener(this);
                 System.out.println("Все содержимое контента вставлено в TextArea");
                 return;
             }
-            if(!delayIsOver)
+            if (!delayIsOver)
                 return;
-            int maxValue=((JScrollBar) e.getSource()).getMaximum();
-            int curValue=e.getValue();
-            if((float) curValue/maxValue>0.95){
-                delayIsOver=false;
+            int maxValue = ((JScrollBar) e.getSource()).getMaximum();
+            int curValue = e.getValue();
+            if ((float) curValue / maxValue > 0.95) {
+                delayIsOver = false;
                 Thread doDelayThread = new Thread(artificialDelay);
                 doDelayThread.start();
-                startSymbol=finishSymbol;
-                finishSymbol+=step;
-                insertContentPart(startSymbol,finishSymbol);
-                System.out.println(startSymbol+"ОТ ДО"+finishSymbol);
+                startSymbol = finishSymbol;
+                finishSymbol += step;
+                insertContentPart(startSymbol, finishSymbol);
+                System.out.println(startSymbol + "ОТ ДО" + finishSymbol);
             }
         }
     }
-    class ContentChangedTrue implements DocumentListener{
+
+    class ContentChangedTrue implements DocumentListener {
 
         @Override
         public void insertUpdate(DocumentEvent e) {
-            contentChanged=true;
+            contentChanged = true;
         }
 
         @Override
         public void removeUpdate(DocumentEvent e) {
-            contentChanged=true;
+            contentChanged = true;
         }
 
         @Override
         public void changedUpdate(DocumentEvent e) {
-            contentChanged=true;
+            contentChanged = true;
         }
     }
 }
@@ -141,18 +146,20 @@ public class TextViewer extends JPanel {
 /**
  * Класс, реализующий задержку в пол секунды перед изменением флага textViewer.delayIsOver, означающим, что можно вставить часть контента
  */
-class ArtificialDelay implements Runnable{
+class ArtificialDelay implements Runnable {
     TextViewer textViewer;
-    public ArtificialDelay(TextViewer textViewer){
-        this.textViewer=textViewer;
+
+    public ArtificialDelay(TextViewer textViewer) {
+        this.textViewer = textViewer;
     }
-    public synchronized void run(){
+
+    public synchronized void run() {
         try {
             TimeUnit.MILLISECONDS.sleep(500);
-            textViewer.delayIsOver=true;
+            textViewer.delayIsOver = true;
             textViewer.scrollPane.getVerticalScrollBar().setValue((int) (textViewer.scrollPane.getVerticalScrollBar().getMaximum() * 0.90));
         } catch (InterruptedException e1) {
-            textViewer.delayIsOver=true;
+            textViewer.delayIsOver = true;
         }
     }
 }
@@ -160,32 +167,34 @@ class ArtificialDelay implements Runnable{
 /**
  * Класс, который читает файл в отдельном потоке в переменную fileContent объекта TextViewer
  */
-class ReadingFileThread implements Runnable{
+class ReadingFileThread implements Runnable {
     File file;
     TextViewer textViewer;
-    public ReadingFileThread(File file,TextViewer textViewer){
-        this.file=file;
-        this.textViewer=textViewer;
-    }
-    public void run(){
-        try {
-            textViewer.fileContent=read(file);
-            textViewer.insertContentPart(textViewer.startSymbol,textViewer.finishSymbol);
 
-        }catch(IOException e){
+    public ReadingFileThread(File file, TextViewer textViewer) {
+        this.file = file;
+        this.textViewer = textViewer;
+    }
+
+    public void run() {
+        try {
+            textViewer.fileContent = read(file);
+            textViewer.insertContentPart(textViewer.startSymbol, textViewer.finishSymbol);
+
+        } catch (IOException e) {
             //todo закрытие таба
         }
     }
+
     public synchronized static String read(File file) throws IOException {
         StringBuilder sb = new StringBuilder();
         try {
             BufferedReader in = new BufferedReader(new FileReader(file.getAbsoluteFile()));
             String s;
-            while ((s = in.readLine()) != null){
+            while ((s = in.readLine()) != null) {
                 sb.append(s);
                 sb.append("\n");
             }
-
             in.close();
         } catch (IOException e) {
             throw new IOException(e);
