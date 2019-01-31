5497466016dfb759e089a71506c0fc40a95b5386
tenshi-boy <stanislawchics@gmail.com>
2019-02-01 00:26:08 +0300

    Убрал мусор

--- src/my/log/searcher/LogStarter.java
+++ src/my/log/searcher/LogStarter.java
@@ -1,11 +1,7 @@
 package my.log.searcher;
 
-import java.io.File;
-
 public class LogStarter {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         new MainWindow();
-      //  new ReplaceWindow(new TextViewer(new File("asd")));
     }
 }
